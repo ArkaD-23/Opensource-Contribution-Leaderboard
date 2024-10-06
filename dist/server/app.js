c33286c1185e8fd63feffd30e4b30e6de7a537a0
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-10-06 20:12:46 +0530

    localhost test

--- dist/server/app.js
+++ dist/server/app.js
@@ -17,12 +17,12 @@ const logPath = '../assets/data/log.json'
 const port = jsonfile.readFileSync(configPath).serverPort
 const configBackupPath = '../../configBackup.json'
 const proxyOption = {
-    target: 'https://gsoc-heat-leaderboard.onrender.com' + '/',
+    target: 'http://localhost:' + port + '/',
     pathRewrite: { '^/api': '' },
     changeOrigin: true,
 }
 const websocketProxyOption = {
-    target: 'https://gsoc-heat-leaderboard.onrender.com' + '/',
+    target: 'http://localhost:' + port + '/',
     changeOrigin: true,
 }
 
